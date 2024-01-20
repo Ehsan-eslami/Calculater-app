3439aa427213dacb1dc6e76948aa7342a2fb73e8
EHSAN\esehs <eslami.ehsan200@gmail.com>
2024-01-20 17:03:30 +0330

    create a app

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      >Frontend Mentor</a
+    >. Coded by
+    <a href="https://www.frontendmentor.io/profile/Ehsan-eslami">Ehsan Eslami</a
+    >.
+  </div>
+</template>
